00baa4f0b05457cd22d219fd4925b07a1b807426
wallarug <wallarug@coliemore.com.au>
2020-01-16 22:47:54 +1100

    Still having issues.

--- examples/bluetoothuart_simpletest.py
+++ examples/bluetoothuart_simpletest.py
@@ -1,5 +1,7 @@
 import board
 import busio
+
+from digitalio import DigitalInOut, Direction, Pull
 from time import sleep
 
 #from roboticsmasters_bluetoothuart import BluetoothUART
@@ -7,13 +9,32 @@ from time import sleep
 
 uart = busio.UART(board.GPS_TX, board.GPS_RX, baudrate=9600)
 
-at = b'AT'
+enable = DigitalInOut(board.SDA)
+enable.direction = Direction.OUTPUT
 
-uart.write(at)
+enable.value = True
+
+#state = DigitalInOut(board.SCL)
+#state.direction = Direction.INPUT
+#print(state.value)
+
+at = b'AT+HELP\r\n'
+rt = b'\r\n'
+
+#uart.write(at)
 
 
 while True:
+    
+    uart.write(at)
     data = uart.readline()
+    #uart.write(at)
 
     if data is not None:
         print("data: ", data)
+
+    sleep(1)
+
+#bt = BluetoothUART(uart)
+
+#print(bt.connected)
